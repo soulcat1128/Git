ce6d60907db3edc4b9f427ffff06f100580e71fd
JasonLin9123 <t109590001@ntut.org.tw>
2022-03-18 15:25:30 +0800

    乙新增7B練習

--- GitTutorial/GitTutorial/GitTutorial.cpp
+++ GitTutorial/GitTutorial/GitTutorial.cpp
@@ -4,6 +4,14 @@
 #include <cstdlib>
 
 bool Guess(int number) {
+	static int target = -1;
+
+	srand(time(NULL));
+	if (target == -1) {
+		target = rand() % 100 + 1;
+	}
+	if (number == target)
+		return true;
 	return false;
 }
 
