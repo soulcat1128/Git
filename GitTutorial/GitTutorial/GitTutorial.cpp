1e55bac1a048abb1da5d9f52f8122bb2cea9218a
soulcat69 <t109590021@ntut.edu.tw>
2022-03-18 17:48:15 +0800

    最後一步

--- GitTutorial/GitTutorial/GitTutorial.cpp
+++ GitTutorial/GitTutorial/GitTutorial.cpp
@@ -12,7 +12,17 @@ bool Guess(int number) {
 	}
 	if (number == target)
 		return true;
-	return false;
+
+	if (number > target) {
+		std::cout << "Smaller" << std::endl;
+		return false;
+	}
+	else if (number < target)
+	{
+		std::cout << "Bigger" << std::endl;
+		return false;
+	}
+	return true;
 }
 int main() 
 {
